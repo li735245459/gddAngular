aded6644bc3af24f1921dabd8a4117ea7b85657c
li735245459 <lixing_java@163>
2018-06-13 15:21:17 +0800

    lixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,8 @@
 import {Component} from '@angular/core';
-import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  menuText = '首页';
-
-  constructor(public router: Router) {}
-
-  /**
-   * 菜单按钮事件
-   * @param event
-   */
-  onMenu(event): void {
-    this.menuText = event.target.text;
-  }
-}
+export class AppComponent {}
