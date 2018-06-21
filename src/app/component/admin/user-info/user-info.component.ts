ad802204d4a95e2c609c7ee524ee39c7c0682b49
li735245459 <lixing_java@163>
2018-06-21 12:41:10 +0800

    fdg

--- src/app/component/admin/user-info/user-info.component.ts
+++ src/app/component/admin/user-info/user-info.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {DataGridModule} from 'ng-easyui/components/datagrid/datagrid.module';
 
 import {UserService} from '../../../service/user.service';
 
@@ -10,29 +9,115 @@ import {UserService} from '../../../service/user.service';
 })
 export class UserInfoComponent implements OnInit {
   msg = '查询成功';
-  data = [];
-  total = 100;
-  // pageSize = 10;
-  // pageNumber = 1;
-  pageOptions = {
-    layout: ['list', 'sep', 'first', 'prev', 'next', 'last', 'sep', 'tpl', 'info']
+  selectionRows; // 选中的行
+  data; // 数据
+  total; // 所有数据大小
+  pageSize = 20; // 分页大小
+  pageNumber = 1; // 当前分页
+  pageOptions = { // 分页导航设置
+    pageList: [10, 20, 40],
+    displayMsg: '当前 {from} 到 {to} , 共 {total} 条',
+    layout: ['list', 'sep', 'first', 'prev', 'next', 'last', 'sep', 'tpl', 'info'] // 报错
   };
+  loading = true;
+  loadMsg = '数据正在加载..';
 
   constructor(
     private userService: UserService) {
   }
 
   ngOnInit() {
+    this.queryByPage();
+  }
+
+  /**
+   * 分页查询
+   */
+  queryByPage() {
     this.userService.queryByPage().subscribe(responseJson => {
       if (responseJson.code === 0) {
         this.msg = '查询成功';
         this.data = responseJson.data;
+        this.total = 100;
+        this.loading = false;
       } else {
         this.msg = '查询失败';
       }
     });
   }
 
+  /**
+   * 分页插件回调事件
+   * @param event
+   */
+  onPageChange(event) {
+    this.pageNumber = event.pageNumber;
+    this.pageSize = event.pageSize;
+    this.queryByPage();
+  }
 
+  /**
+   * row 样式定义
+   * @param row
+   */
+  setRowCss(row) {
+    if (row.email === 'lisi13_java@163.com') {
+      return {background: '#b8eecf', fontSize: '14px', fontStyle: 'italic'};
+    }
+    return null;
+  }
+
+  /**
+   * sex cell 样式定义
+   * @param row
+   * @param value
+   * @returns {{color: string}}
+   */
+  setSexCellCss(row, value) {
+    if (value === 'male') {
+      return {color: 'blue'};
+    } else {
+      return {color: 'red'};
+    }
+  }
+
+  /**
+   * 取消选择
+   */
+  onRedo(): void {
+    this.selectionRows = null;
+  }
+
+  /**
+   * 删除
+   */
+  onRemove(): void {
+    if (this.selectionRows) {
+      console.log(this.selectionRows.map(row => row.id).join(','));
+    } else {
+      console.log('请选中需要删除的数据');
+    }
+  }
+
+  /**
+   * 清空所有
+   */
+  onRemoveAll(): void {
+    console.log('删除所有数据');
+  }
+
+  /**
+   * 添加
+   */
+  onAdd(): void {
+
+  }
+
+  /**
+   * 刷新数据
+   */
+  onReLoad(): void {
+    this.queryByPage();
+  }
 
 }
